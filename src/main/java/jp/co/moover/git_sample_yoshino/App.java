4327cc922cdb65549bd7ab060ffc39111cfaf1d9
yoshinoA <atsuko.yoshino@rakus.co.jp>
2017-08-08 11:48:07 +0900

    新規プロジェクトの作成

---
+++ src/main/java/jp/co/moover/git_sample_yoshino/App.java
@@ -0,0 +1,13 @@
+package jp.co.moover.git_sample_yoshino;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
